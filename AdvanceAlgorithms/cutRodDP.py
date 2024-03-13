d92a67105f9aea045f19fbfaab2f5a01d49fe6fc
dineshkumarkolimi <60639261+dineshkumarkolimi@users.noreply.github.com>
2024-03-13 15:27:49 +0530

    Update cutRodDP.py

--- AdvanceAlgorithms/cutRodDP.py
+++ AdvanceAlgorithms/cutRodDP.py
@@ -3,6 +3,11 @@
     cut a rod of given lengths in such a way that it gives maximum profit
     by cutting them in given possible proportions.
 
+    Given a rod of length n inches and an array of prices that includes prices of all pieces of size smaller than n. 
+    Determine the maximum value obtainable by cutting up the rod and selling the pieces. 
+    For example, if the length of the rod is 8 and the values of different pieces are given as the following, 
+    then the maximum obtainable value is 22 (by cutting in two pieces of lengths 2 and 6) 
+
     Dynamic Programming
     Complexity: O(n^2)
 """

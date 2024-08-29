e7f6b4067902b6a8d1eaef8877301360d57fcdfc
DineshkumarKolimi40 <dinesh.kolimi@heeddata.com>
2024-08-29 23:27:14 +0530

    adding concepts related to multiprocessing and threading

--- conceptsPython/concurrency_parallelism.py
+++ conceptsPython/concurrency_parallelism.py
@@ -4,9 +4,24 @@
 # When tasks involve waiting for external resources (like disk I/O or network I/O), 
 # using threads (threading, concurrent.futures.ThreadPoolExecutor) or asyncio can 
 # significantly improve performance.
+
 # CPU-bound tasks:
 # When tasks involve heavy computations, using processes (multiprocessing, 
 # concurrent.futures.ProcessPoolExecutor) can take advntage of multiple CPU cores for parallel processing
+
+# Key Concepts
+# Global Interpreter Lock (GIL): Python’s GIL ensures that only one thread executes Python bytecode
+# at a time, which limits the performance of CPU-bound multi-threaded applications. 
+# To bypass this limitation, use multiprocessing.
+
+# Event Loop: In asyncio, the event loop runs asynchronous tasks in an efficient, non-blocking manner.
+
+# Summary
+# Concurrency: Manage multiple tasks making progress over time (e.g., using threads or asyncio).
+# Parallelism: Run multiple tasks at the same time on different CPU cores (e.g., using multiprocessing).
+# Libraries: threading, multiprocessing, concurrent.futures, asyncio are the main libraries for handling concurrency and parallelism in Python.
+# GIL: Limits CPU-bound multi-threaded performance; use multiprocessing to overcome it.
+# Use Cases: Choose threading/asyncio for I/O-bound tasks and multiprocessing for CPU-bound tasks.
 
 import threading
 from concurrent.futures import ThreadPoolExecutor, as_completed
